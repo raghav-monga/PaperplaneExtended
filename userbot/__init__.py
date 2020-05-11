ce676bf70579843bd726812cbc9da5a5be2313f6
Sammy0062 <65174221+Sammy0062@users.noreply.github.com>
2020-05-11 19:20:49 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -84,7 +84,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/Kaneki993/OpenUserBot.git")
+    "https://github.com/Sammy0062/OpenUserBot.git")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
